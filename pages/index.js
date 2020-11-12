a3ad91865f94f565cbf3fbf97181e49ad053ffbb
Leroy Wan <leroywan@gmail.com>
2020-11-12 15:19:20 -0800

    Add material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import styles from './app.module.scss';
 
+import Button from '@material-ui/core/Button';
+
 export default function Home() {
   return (
     <div>
       <h1 className={styles.Test}>Hello world</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   )
 }
